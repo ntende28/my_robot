e7acf1e3a670225979abda39e1375360ac5244b7
ntende28 <ed25ntende@gmail.com>
2025-02-06 14:10:09 +0800

    updating files to enable the odom_publisher node

--- my_robot_bringup/scripts/odom_publisher.py
+++ my_robot_bringup/scripts/odom_publisher.py
@@ -1,48 +1,54 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # importing the necessary dependencies
 import rospy
 from nav_msgs.msg import Odometry
-from std_msgs import Header
+from std_msgs.msg import Header
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 
-# creating our odom publisher node
-rospy.init_node("odom_publisher")
+def main():
 
-# creting the publisher object with Odometry msg type, and queue - 10msgs
-odom_publisher = rospy.Publisher("/my_odom", Odometry, 10)
+    # creating our odom publisher node
+    rospy.init_node("odom_publisher")
 
-# waiting for the service to start i.e. service -> gazebo/get_model_state
-rospy.wait_for_service("/gazebo/get_model_state")
-#creating a proxy to call the service of type GetModeState
-get_model_srv = rospy.ServiceProxy("/get_model_state", GetModelState)
+    # creting the publisher object with Odometry msg type, and queue - 10msgs
+    odom_publisher = rospy.Publisher("/my_odom", Odometry, queue_size=10)
 
-# creating the odom headers
-odom = Odometry()
-header = Header()
-header.frame_id = 'odom_frame'
+    # waiting for the service to start i.e. service -> gazebo/get_model_state
+    rospy.wait_for_service("/gazebo/get_model_state")
+    #creating a proxy to call the service of type GetModeState i.e. service -> gazebo/get_model_state
+    get_model_srv = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
 
-# creating the model object
-model = GetModelStateRequest()
-model.model_name = 'my_robot_description'
+    # creating the odom headers
+    odom = Odometry()
+    header = Header()
+    header.frame_id = '/odom'
 
-# publishing frequency
-r = rospy.Rate()
+    # creating the model object
+    model = GetModelStateRequest()
+    model.model_name = 'my_robot_description'
 
-while not rospy.is_shutdown():
-    # get result returned from the model by calling the proxy service
-    result = get_model_srv(model)
+    # publishing frequency
+    r = rospy.Rate(3)
 
-    # set the odom message
-    odom.pose.pose = result.pose
-    odom.twist.twist = result.twist
+    while not rospy.is_shutdown():
+        # get result returned from the model by calling the proxy service
+        result = get_model_srv(model)
 
-    # updating the message headers
-    header.stamp = rospy.Time.now()
-    odom.header = header
+        # set the odom message
+        odom.pose.pose = result.pose
+        odom.twist.twist = result.twist
 
-    # publishing our message using the publisher object
-    odom_publisher.publish(odom)
+        # updating the message headers
+        header.stamp = rospy.Time.now()
+        odom.header = header
 
-    # creating a time delay
-    r.sleep()
+        # publishing our message using the publisher object
+        odom_publisher.publish(odom)
+
+        # creating a time delay
+        r.sleep()
+        # rospy.spin()
+
+if __name__ == "__main__":
+    main()
